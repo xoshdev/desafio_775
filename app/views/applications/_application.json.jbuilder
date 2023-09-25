6827c4c40f681bb7b1f6954058089d16c1851b4a
Joch <xoshdev@gmail.com>
2023-09-25 00:44:19 -0300

    Deployment - Stage 1

---
+++ app/views/applications/_application.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! application, :id, :user_id, :job_id, :created_at, :updated_at
+json.url application_url(application, format: :json)
---
+++ db/migrate/20230925033526_create_jobs.rb
@@ -0,0 +1,11 @@
+class CreateJobs < ActiveRecord::Migration[7.0]
+  def change
+    create_table :jobs do |t|
+      t.string :title
+      t.text :description
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
